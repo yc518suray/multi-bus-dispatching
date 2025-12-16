95d1fc2fb93b2889a88de1574ced79f6a769304b
Raymond_Su <raymondsu0110@gmail.com>
2025-12-16 17:10:37 +0800

    add I/O of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,60 @@ using namespace std;
 
 /* ----- declarations ----- */
 
+int cost();
+
 /* ----- main ----- */
 
 int main(int argc, char ** argv)
 {
 	int opt_data = stoi(argv[2]);
-
-	/* ----- parameters (GA) ----- */
-
-	/* ----- parameters (HS) ----- */
+	int algoOpt = 1; // opt for GA
 
 	/* ----- load data ----- */
-	
-	string line;
+
+	int N;					// prediction steps
+	int Ns;					// number of stops
+	int Nb;					// number of buses
+	double Delta;			// minimum dispatching interval
+	double Tr;				// round trip time (without stopping)
+	double DT;				// dwell time at each stop
+	double Hp;				// prediction horizon
+	int Capb;				// bus capacity
+
 	string infileName;
 	sprintf(infileName, "input-%d.dat", opt_data);
-
 	ifstream infile(infileName);
-	while(!infile.eof())
+
+	infile >> N >> Delta >> Tr >> DT >> Capb >> Hp >> Ns >> Nb;
+
+	int f[N][Ns][Ns] = {{INT_MAX}};// number of psngers coming between trips
+	int numLine = ;				   // expected number of lines to input
+	int x, y, z;
+	for(int i = 0; i < numLine; i++)
 	{
+		infile >> x >> y >> z;
+		if(z == 1)
+		{
+			z = Ns - y + 1;
+		}
+		else
+		{
+			z = z - y;
+		}
+		infile >> f[x][y - 1][z - 1];
 	}
+	
 	infile.close();
+
+	/* ----- parameters (GA) ----- */
+	
+	int Np = 50;		// population size
+	int Nit = 1000;		// number of iteration
+	int chromosome[Np][Ns][Ns] = {{0}};
+
+	/* ----- parameters (HS) ----- */
+	
+	/* ----- pending ------ */
 
 	/* ----- Opt 1: Genetic Algorithm ----- */
 	if(algoOpt == 1)
@@ -35,6 +68,7 @@ int main(int argc, char ** argv)
 	/* ----- Opt 2: Harmony Search ----- */
 	else if(algoOpt == 2)
 	{
+		/* ----- pending ----- */
 	}
 	else
 	{
@@ -48,3 +82,8 @@ int main(int argc, char ** argv)
 }
 
 /* ----- function definitions ----- */
+
+int cost()
+{
+	/* ----- fitness = 1 / cost ------ */
+}
